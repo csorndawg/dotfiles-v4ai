8c53b386a7d9d5a8547c1702f0529a1bdb2b3e0e
csorndawg <zaccsorny@yahoo.com>
2026-01-15 04:38:00 +0000

    Add broken/partial working code for runtime overrides

--- utils.d/source_rc_utils.sh
+++ utils.d/source_rc_utils.sh
@@ -1,16 +1,38 @@
-
-# Sourcing script for all runtime config submodules
+# Script sources all "utils.d" runtime config submodules from my dotfiles repo
 
 DOTFILE_ROOT="$HOME/dotfiles"
 UTILSD_RGX="\.rc$"
+
+# ignore patterns for any file in utils.d to ignore (after UTILSD_RGX filter)
 UTILSD_IGNR='ignore(me)?|test|experiment|\.sw(ap)'
 
-# sets list of ".rc" submodules that will be sourced
+# .rc files that need to be sourced after all other RC modules are loaded
+RC_SRC_LAST_RGX="utils.d\/*(override|hotfix).+\.rc"
+
+# source runtime modules (unordered/non-determisitic sourcing)
 rcModules=$(find "$DOTFILE_ROOT/utils.d" -type f | egrep "$UTILSD_RGX" \
+	| egrep -v "$UTILSD_IGNR" \
+	| egrep -v "$RC_SRC_LAST_RGX")	# filter out any rc files that need to be loaded at the end
+
+echo ""
+echo "Sourcing Generic Runtime Modules:"
+for runtimeModule in ${rcModules[@]}; do
+	echo "runtimeModule: $runtimeModule"
+	source "$runtimeModule"  || echo "Error occurred while sourcing \"$runtimeModule\""
+done
+
+# source runtime modules that MUST be SOURCED LAST (after all other rc modules)
+rcOverrides=$(find "$DOTFILE_ROOT/utils.d" -type f \
+	| egrep "$RC_SRC_LAST_RGX" \
 	| egrep -v "$UTILSD_IGNR")
 
 echo ""
-for rcmod in ${rcModules[@]}; do
-	#echo "rcmod: $rcmod"
-	source "$rcmod"  || echo "Error occurred while sourcing \"$rcmod\""
+echo "Sourcing Runtime Overrides:"
+for overrideModule in ${rcOverrides[@]}; do
+	echo "overrideModule: $overrideModule"
+	source "$overrideModule"  || echo "Error occurred while sourcing \"$overrideModule\""
 done
+
+
+echo ""
+echo "Done sourcing all runtime files"
