f228f1679c1c9920ac07ad16df278db54f8660df
csorndawg <zaccsorny@yahoo.com>
2026-01-22 00:59:36 +0000

    Fix bug in make_tmux_symlinks script

--- tmux.d/make_tmux_symlinks.sh
+++ tmux.d/make_tmux_symlinks.sh
@@ -15,26 +15,32 @@ TARG_DIR="$(dirname "$TARG_PATH")"
 # make symlink dotfiles/tmux.d --> XDG_CONFIG symlink
 echo ""
 if [ -d  "$(dirname "$TARG_PATH")" ]; then
-	mv -vf "$TARG_PATH" "$TARG_PATH.ark"
+	#mv -vf "$TARG_PATH" "$TARG_PATH.ark"
 	ln -sf "$TARG_PATH" "$TARG_PATH"
-	echo "Creating symlink:    $SRC_PATH --> $TARG_PATH"
-
 else
 	echo "No "XDG_CONFIG/tmux" folder exists. Creating symlink from dotfiles to local config."
+	ln -sf "$TARG_PATH" "$TARG_PATH"
+echo "Creating symlink:    $SRC_PATH --> $TARG_PATH"
 fi
 
 # ceates symlink: dotfiles/tmux.d/tmux.conf TMUX.conf  >>  $HOME/.tmux.conf
 SRC_PATH="$DOTFILES_HOME/tmux.d/tmux.conf"
 TARG_PATH="$HOME/.tmux.conf"
 if [ -f  "$TARG_PATH" ]; then
-
 	mv -vf "$TARG_PATH" "$TARG_PATH.ark" 
 	ln -sf "$SRC_PATH" "$TARG_PATH"
-
-	echo "Created symlink:    $SRC_PATH --> $TARG_PATH"
+else
+	echo "No '~/.tmux.conf' file exists. Creating symlink from 'dotfiles/tmux.d/tmux.conf' to $USER/.tmux.conf"
+	ln -sf "$SRC_PATH" "$TARG_PATH"
+echo "Created symlink:    $SRC_PATH --> $TARG_PATH"
 fi
 
 # after generating requried/necessary symlinks 
 TMUX_DEPENDS_SCRIPT="$DOTFILES_HOME/tmux.d/tmux_conf_dependencies.sh"
 echo Running: "bash \"$TMUX_DEPENDS_SCRIPT\""
 bash "$TMUX_DEPENDS_SCRIPT"
+
+# clean up extra symlink thats created on accident
+SYML_PATH="$DOTFILES_HOME/tmux.d/tmux"
+rm -v "$SYML_PATH"
+echo "Removed extra, self-referencing symlink"
