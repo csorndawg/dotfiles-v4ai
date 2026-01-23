1a5b5ceba0b3304fa277f1b3a92695da7428bfb0
csorndawg <zaccsorny@yahoo.com>
2026-01-23 14:41:07 +0000

    Add nvim-kickstart aliases. Add logic to source RC overrides after other RC modules

--- utils.d/source_rc_utils.sh
+++ utils.d/source_rc_utils.sh
@@ -8,9 +8,28 @@ UTILSD_IGNR='ignore(me)?|test|experiment|\.sw(ap)'
 # sets list of ".rc" submodules that will be sourced
 rcModules=$(find "$DOTFILE_ROOT/utils.d" -type f | egrep "$UTILSD_RGX" \
 	| egrep -v "$UTILSD_IGNR")
-
-echo ""
+echo "Sourcing RC modules from \"$DOTFILE_ROOT/utils.d\""
 for rcmod in ${rcModules[@]}; do
 	#echo "rcmod: $rcmod"
-	source "$rcmod"  || echo "Error occurred while sourcing \"$rcmod\""
+	#source "$rcmod"  1>& /dev/null
+	#echo bash "$rcmod"  1>& /dev/null
+	source "$rcmod"  1>& /dev/null
 done
+echo "Done sourcing RC modues"
+
+# now source experimental/overrides runtime configurations
+RC_OVRD_RGX="experiment|override"
+RC_OVRD_IGNR="\.sw(a)p\b"
+rcModules=$(find "$DOTFILE_ROOT/utils.d" -type f | egrep "$UTILSD_RGX" \
+	| egrep -v "$RC_OVRD_IGNR" \
+	| egrep "$RC_OVRD_RGX")
+
+echo ""
+echo "Sourcing RC overrides"
+for rcmod in ${rcModules[@]}; do
+	#echo "override file: $rcmod"
+	#echo "source command:  source \"$rcmod\""
+	source "$rcmod" 1>& /dev/null
+done
+echo "Done sourcing RC overrides"
+echo ""
