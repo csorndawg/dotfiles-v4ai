4861c6dff933966f8d4462fd2ae220dca7a7808f
csorndawg <zaccsorny@yahoo.com>
2026-01-26 05:45:22 +0000

    Add nvim plugins/lsp.lua file. This installs mason.nvim, mason-lspconfig.nvim, nvmi-lspconfig and configures LSP servers for the specified servers.

--- nvim.d/lua/custom/opts.lua
+++ nvim.d/lua/custom/opts.lua
@@ -69,3 +69,17 @@ opt.wrap = false -- Disable line wrap
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
+
+
+-- SSH remote clipboard intergration (Gemini)
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
