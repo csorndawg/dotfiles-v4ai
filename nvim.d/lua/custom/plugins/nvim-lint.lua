7cf849e88839b743caf39f906eff6dd3a89cf8a2
csorndawg <zaccsorny@yahoo.com>
2026-02-05 06:59:34 +0000

    Upgraded nvim-lint - added a few new file types and tested keymaps/autocmds

--- nvim.d/lua/custom/plugins/nvim-lint.lua
+++ nvim.d/lua/custom/plugins/nvim-lint.lua
@@ -11,6 +11,9 @@ return {
 				bash = { "shellcheck" },
 				yaml = {},
 				docker = {},
+        --toml ={"taplo"},
+        toml = {"taplo"},
+        cpp = {"cpplint"},
         sql = {"sqlfluff"},
 			}
 
@@ -31,7 +34,7 @@ return {
 				callback = function()
 					-- Check filetype and lint level
 					local ft = vim.bo.filetype
-					local supported_filetypes = { bash = true, python = true, yaml = true, docker = false, toml = true }
+					local supported_filetypes = { bash = true, python = true, yaml = true, cpp = true, sql = true, docker = false, toml = true }
 
 					-- Only run the linter in buffers that you can modify and are supported
 					if supported_filetypes[ft] and vim.opt_local.modifiable:get() then
@@ -43,7 +46,24 @@ return {
 				end,
 			})
 
-			-- Keymaps to toggle linting levels
+      -- Show diagnostics in floating window when cursor holds on a line
+      vim.api.nvim_create_autocmd("CursorHold", {
+          callback = function()
+              vim.diagnostic.open_float(nil, { 
+                  focusable = false,
+                  close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                  border = "rounded",
+                  source = "always",
+                  prefix = " ",
+              })
+          end
+      })
+
+    -- Adjust how quickly the float appears (in milliseconds)
+      vim.opt.updatetime = 500  -- 500ms delay
+
+      --
+      -- LINTER Keymaps
 			vim.api.nvim_set_keymap(
 				"n",
 				"<leader>li",
@@ -65,25 +85,59 @@ return {
 				{ noremap = true, silent = true, desc = "Lint Level: Error (Disabled)" }
 			)
 
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>lv",
-				-- ":lua update_lint_level('warning')<CR>",
-				function()
-          -- 2 lines below are syntaxically equivalent (one is just syntax sugar?)
-          -- "<cmd> require('lint').<plugin function>() <cr>"
-          -- require('<plugin name>').<plugin function>
-          -- example: 
-          -- -- vim.keymap.set("n", "zR", "<cmd> require('ufo').openAllFolds() <cr>", { desc = "UFO Fold Open" })
-          -- -- vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "UFO Fold Open" })
-          local linters = require("lint").get_running()
-          if #linters == 0 then
-            return ">> "
-          end
-          return ">> " .. table.concat(linters,"|")
+      -- shows linters are configured for current buffer file type
+      -- AND which ones are currently running
+      vim.keymap.set(
+        "n",
+        "<leader>lv",
+        function()
+            local linters = require("lint").get_running()
+            local configured = require("lint").linters_by_ft[vim.bo.filetype] or {}
+            
+            local msg = string.format(
+                "Configured: [%s] | Running: [%s]",
+                table.concat(configured, ", "),
+                #linters > 0 and table.concat(linters, ", ") or "none"
+            )
+            
+            vim.notify(msg, vim.log.levels.INFO)
         end,
-				{ noremap = true, silent = true, desc = "View active linters" }
-			)
+        { noremap = true, silent = true, desc = "View active linters" }
+      )
+
+      -- forcefully shows linter diagnostics
+      vim.keymap.set(
+        "n",
+        "<leader>ld",
+        function()
+            vim.diagnostic.open_float(nil, { 
+                border = "rounded",
+                source = "always",
+            })
+        end,
+        { desc = "Show line diagnostics" }
+    )
+
+      -- @NOTE: DONT DELETE -- using this as reference for tweaking lazy lua plugins structure
+		-- 	vim.keymap.set(
+		-- 		"n",
+		-- 		"<leader>lv",
+		-- 		-- ":lua update_lint_level('warning')<CR>",
+		-- 		function()
+  --         -- 2 lines below are syntaxically equivalent (one is just syntax sugar?)
+  --         -- "<cmd> require('lint').<plugin function>() <cr>"
+  --         -- require('<plugin name>').<plugin function>
+  --         -- example: 
+  --         -- -- vim.keymap.set("n", "zR", "<cmd> require('ufo').openAllFolds() <cr>", { desc = "UFO Fold Open" })
+  --         -- -- vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "UFO Fold Open" })
+  --         local linters = require("lint").get_running()
+  --         if #linters == 0 then
+  --           return ">> "
+  --         end
+  --         return ">> " .. table.concat(linters,"|")
+  --       end,
+		-- 		{ noremap = true, silent = true, desc = "View active linters" }
+		-- 	)
 		end,
 	},
 }
