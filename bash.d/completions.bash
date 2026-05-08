8e703d0bdbfb2d5dec22035fb27e92a03a5d5e91
csorndawg <zaccsorny@yahoo.com>
2026-05-08 08:24:55 -0400

    Add logic to ignore/skip sourcing certain files bashd completion files

--- bash.d/completions.bash
+++ bash.d/completions.bash
@@ -4,8 +4,17 @@
 DOTFILES_DIR="$HOME/dotfiles"
 BASHD_COMP_DIR="$DOTFILES_DIR/bash.d/completions"
 
-for f in $(find "$BASHD_COMP_DIR" -maxdepth 1 -type f); do
-	source "$f" 1> /dev/null
+# source all bash.d completion files except ones with ignored file type pattern
+for f in $(find "$BASHD_COMP_DIR" -maxdepth 1 -type f); do 
+  # echo -e "\nFile:\t $f"
+  skipFlag=$(echo $f | egrep -c '\.(zig|ZIG|ignore|ignored|wip)$')
+  if (( $skipFlag > 0 )); then
+    # echo "Skipping file:  $file"
+    continue
+  else
+    source "$f" 1> /dev/null
+    # echo -e "\nSourced:  $f"
+  fi
 done
-echo "Finished sourcing bash.d/completion scripts"
-echo ""
+
+echo -e "\nSuccesfully sourced bash.d/completion scripts"
