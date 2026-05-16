438e88b9f8ee540402bcf1ef9f093b7e05190776
csorndawg <zaccsorny@yahoo.com>
2026-05-16 11:47:22 +0000

    Dadbod tweaks

--- nvim.d/lua/plugins/dadbod.lua
+++ nvim.d/lua/plugins/dadbod.lua
@@ -11,9 +11,22 @@ return {
       'DBUIAddConnection',
       'DBUIFindBuffer',
     },
+    keys = {
+      -- { 'n', '<Leader>D', '<cmd>DBUIToggle<Cr>', { desc = 'DBUI Toggle' } },
+      -- { '<Leader>Dt', '<cmd>DBUIToggle<Cr>', { desc = 'DBUI Toggle' } },
+      { '<Leader>Dt', '<cmd>DBUIToggle<Cr>', desc = 'DBUI Toggle' },
+
+    },
     init = function()
       -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
+      local data_path = vim.fn.stdpath 'data'
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. '/dadbod'
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. '/dadbod/tmp'
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+      vim.g.db_ui_execute_on_save = true
     end,
   },
 }
