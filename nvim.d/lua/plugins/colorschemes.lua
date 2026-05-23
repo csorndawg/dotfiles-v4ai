3917bd78aa0e5b6e90b2476e1a54c3be8e454ad6
csorndawg <zaccsorny@yahoo.com>
2026-05-23 22:45:03 +0000

    Fix nord

--- nvim.d/lua/plugins/colorschemes.lua
+++ nvim.d/lua/plugins/colorschemes.lua
@@ -1,7 +1,7 @@
 -- Nvim colorschemes lazy loader
 return {
   -- lazy-load colorschemes so they don't collisions at startup
-  { "shaunsingh/nord.nvim", lazy = true },
+  -- { "shaunsingh/nord.nvim", lazy = true },
   { "arcticicestudio/nordic.nvim", lazy = true },
   { "shaunsingh/onenord.nvim", lazy = true },
 }
