1617bea87c02903f385215d971699a926af5e0b5
csorndawg <zaccsorny@yahoo.com>
2026-06-09 02:18:05 -0400

    Add select lazynvim autocmds

--- nvim.d/lua/custom/autocmds.lua
+++ nvim.d/lua/custom/autocmds.lua
@@ -2,6 +2,10 @@
 -- Custom Nvim Autocmds
 -----------------------------------------------
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("zc_" .. name, { clear = true })
+end
+
 --
 -- Highlight yanked text
 --
@@ -56,4 +60,46 @@ vim.filetype.add({
   extension = {
     rc = "bash",
   },
+})
+
+
+--- resize splits automatically if window gets resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- 
+-- go to last location when reopening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].zc_last_loc then
+      return
+    end
+    vim.b[buf].zc_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- auto create missing dir path components when saving a file
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
 })
