ddc3824afbcf3dd128b35f6cbdd6e7b64eb2030d
csorndawg <zaccsorny@yahoo.com>
2026-06-10 02:47:28 +0000

    Add nvim bashrc alias

--- bash.d/aliases.bash
+++ bash.d/aliases.bash
@@ -1,3 +1,7 @@
 # BASHRC Aliases Submodule
 
 alias dot="cd $HOME/dotfiles"
+
+# use venv-nvimd as [d]efault [v]en[v]
+alias vvn="source /home/sftp1/.config/venvs/venv-nvimd/bin/activate"
+alias vvd="source /home/sftp1/.config/venvs/venv-nvimd/bin/activate"
